9f20613662a0dfbd943b8ff5111db7c82ff51cb1
rododenron <nextx@rambler.ru>
2024-01-02 12:32:26 +0700

    D9.5

--- NewsPortsl/News/urls.py
+++ NewsPortsl/News/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 # Импортируем созданное нами представление
 from .views import PostsList, PostDetail, PostSearch, PostCreate, PostEdit, PostDelete, upgrade_me, CategoriesList, \
    subscribe
 
 urlpatterns = [
-   path('', PostsList.as_view(), name='post_list'),
-   path('<int:pk>', PostDetail.as_view(), name='post_detail'),
+   path('', cache_page(60)(PostsList.as_view()), name='post_list'),
+   path('<int:pk>', cache_page(300)(PostDetail.as_view()), name='post_detail'),
    path('search', PostSearch.as_view(), name='post_search'),
    path('create', PostCreate.as_view(), name='post_create'),
    path('<int:pk>/edit/', PostEdit.as_view(), name='post_edit'),
