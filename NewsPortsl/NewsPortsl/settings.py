b6044e49e5051594ac35045fa5a20b69d997acac
rododenron <nextx@rambler.ru>
2024-04-11 09:41:40 +0700

    prepare to translate

--- NewsPortsl/NewsPortsl/settings.py
+++ NewsPortsl/NewsPortsl/settings.py
@@ -52,6 +52,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'django.middleware.locale.LocaleMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -284,3 +287,7 @@ LOGGING = {
         },
     }
 }
+
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
